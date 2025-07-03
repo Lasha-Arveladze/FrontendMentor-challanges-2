dda6379206ea5babdadb265b798755f2d0e61849
Lasha Arveladze <larveladze.tech@gmail.com>
2025-07-03 20:33:15 +0400

    finished project

--- arch-studio-multi-page-website/dist/starter-code.ad9a409f.js
+++ arch-studio-multi-page-website/dist/starter-code.ad9a409f.js
@@ -3,6 +3,13 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const swiper = new Swiper(".swiper", {
         direction: "horizontal",
         loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            renderBullet: function(index, className) {
+                return '<div class="' + className + '">' + (index + 1 < 10 ? "0" : "") + (index + 1) + "</div>";
+            }
+        },
         autoplay: {
             delay: 5000,
             disableOnInteraction: false
--- arch-studio-multi-page-website/starter-code/Javascript/index.js
+++ arch-studio-multi-page-website/starter-code/Javascript/index.js
@@ -3,6 +3,21 @@ document.addEventListener("DOMContentLoaded", () => {
   const swiper = new Swiper(".swiper", {
     direction: "horizontal",
     loop: true,
+    pagination: {
+      el: ".swiper-pagination", // This MUST match the class of your pagination HTML element
+      clickable: true,
+      renderBullet: function (index, className) {
+        return (
+          '<div class="' +
+          className +
+          '">' +
+          (index + 1 < 10 ? "0" : "") +
+          (index + 1) +
+          "</div>"
+        );
+      },
+    },
+
     autoplay: {
       delay: 5000,
       disableOnInteraction: false,
